99a35bd07b1d35d3fef1cdf6f31db8259d627cd8
Ebrahim Aharpour <ebrahim.aharpour@gmail.com>
2021-01-10 17:39:10 +0100

    Initial commit!

---
+++ src/main/java/com/aharpour/cloud/run/demo/CloudRunDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aharpour.cloud.run.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudRunDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudRunDemoApplication.class, args);
+    }
+
+}
