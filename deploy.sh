2e0db47ec537b9e1d6da8c41846d394b2d27a829
Ebrahim Aharpour <ebrahim.aharpour@gmail.com>
2021-01-13 16:53:41 +0100

    Improving deployment scripts.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-gcloud beta run services replace --platform=managed --region=europe-west4 knative-service.yaml
+
+set -e
+hash=$(git log -1 --format="%H"| cut -c -7)
+docker push "gcr.io/PROJECT_ID/demo:native-${hash}"
+
+export hash="${hash}"
+envsubst < knative-service.yaml >> service.yaml
+gcloud beta run services replace --platform=managed --region=europe-west4 service.yaml
+rm service.yaml
